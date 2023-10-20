57a2605596d61028f26c9bd01347f1046371aecd
Ariel <ariel.nicolas.aguilar@gmail.com>
2023-10-20 12:16:48 -0300

    Arreglé el envio de formulario

--- validacion-reserva.js
+++ validacion-reserva.js
@@ -14,27 +14,20 @@ let reservarBtn = document.getElementById("reservarBtn");
 
 const formReserva = document.getElementById("form-reserva");
 
-formReserva.addEventListener("submit", handleSubmit());
+formReserva.addEventListener('submit', handleSubmit);
 
-async function handleSubmit(event) {
-    event.preventDefault()
-    const formReserva = new FormData(this)
+async function handleSubmit(e) {
+    e.preventDefault();
+    const form = new FormData(this);
     const response = await fetch(this.action, {
         method: this.method,
-        body: formReserva,
+        body: form,
         headers: {
-            "Accept": "application/json"
+            'Accept': 'application/json'
         }
-    })
-    if (response.ok) {
-        this.reset()
-        alert("Reserva realizada con exito, muchas gracias por elegirnos.")
+    });
+    if(response.ok) {
+        alert('Reserva realizada con éxito!');
+        window.location.href = 'index.html';
     }
-};
-
-document.getElementById('reservarBtn').addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = 'index.html';
-    alert("Reserva realizada con exito, muchas gracias por elegirnos.");
 }
-)
