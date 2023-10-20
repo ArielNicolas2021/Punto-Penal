4c74b21ac1d869f23e88dfa21d624e043ae64762
Ariel <ariel.nicolas.aguilar@gmail.com>
2023-10-20 11:28:14 -0300

    Arreglé el envio de formulario

--- validacion-reserva.js
+++ validacion-reserva.js
@@ -1,48 +1,40 @@
-
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-
 function validarDiaLaborable(inputFecha) {
-let fechaSeleccionada = new Date(inputFecha.value);
-let diaabierto = fechaSeleccionada.getDay();
-if (diaabierto === 0 || diaabierto === 6) {
-document.getElementById("mensajeError").textContent = "Ese dia la cancha no esta abierta";
-inputFecha.value = ""; 
-} else {
-document.getElementById("mensajeError").textContent = "";
+    let fechaSeleccionada = new Date(inputFecha.value);
+    let diaabierto = fechaSeleccionada.getDay();
+    if (diaabierto === 0 || diaabierto === 6) {
+        document.getElementById("mensajeError").textContent = "Ese dia la cancha no esta abierta";
+        inputFecha.value = "";
+    } else {
+        document.getElementById("mensajeError").textContent = "";
+    }
 }
+
+
+let reservarBtn = document.getElementById("reservarBtn");
+
+const formReserva = document.getElementById("form-reserva");
+
+formReserva.addEventListener("submit", handleSubmit());
+
+async function handleSubmit(event) {
+    event.preventDefault()
+    const formReserva = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: formReserva,
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    if (response.ok) {
+        this.reset()
+        alert("Reserva realizada con exito, muchas gracias por elegirnos.")
     }
+};
 
-
-    let reservarBtn = document.getElementById("reservarBtn");
-
-    function reservarBoton(){
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'La reserva ha sido confirmada con éxito.',
-            showConfirmButton: true,
-            timer: 1500,
-        })
-        
-        setTimeout(function() {
-            location.href = "index.html";
-        }, 3000);
-    }
-    
-    reservarBtn.addEventListener('click', ()=> {
-        preventDefault(); // Esto cancela el comportamiento del clic
-        Toastify({
-        
-            text: "Reservado con exito.",
-            
-            duration: 2000,
-            style:{
-                fontFamily: "Verdana",
-                fontSize: "15px",
-                color: "White",
-                border: "1px",
-                borderStyle: "solid",
-                }
-    
-            }).showToast();
-});
+document.getElementById('reservarBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = 'index.html';
+    alert("Reserva realizada con exito, muchas gracias por elegirnos.");
+}
+)
